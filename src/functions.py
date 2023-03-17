d526c3505f257668f06e062a3ede9390d35454df
tdanbo <tobiasd@playdead.com>
2023-03-17 16:28:43 +0100

    Update functions.py

--- src/functions.py
+++ src/functions.py
@@ -5,22 +5,25 @@ import os
 import json
 import constants as cons
 
-'''
+"""
 This secion of the code handles general functions that are used often.
-'''
+"""
+
 
 def create_folder(dir_path):
     isExist = os.path.exists(dir_path)
     if not isExist:
         os.makedirs(dir_path)
 
+
 def read_json(file_path):
     file_json = json.load(open(file_path, "r"))
     return file_json
 
+
 def set_icon(widget, icon, color, width=0):
     qicon = QIcon()
-    if icon in [".png",""]:
+    if icon in [".png", ""]:
         pixmap = QPixmap()
     else:
         pixmap = QPixmap(os.path.join(cons.ICONS, icon))
@@ -41,8 +44,12 @@ def set_icon(widget, icon, color, width=0):
             print(f"Scaled icon: {icon} {width}")
             try:
                 pix_width = width  # set the desired width for the scaled icon
-                height = int(pixmap.height() * (pix_width / pixmap.width()))  # calculate the corresponding height
-                scaled_pixmap = pixmap.scaled(pix_width, height)  # create a new scaled pixmap
+                height = int(
+                    pixmap.height() * (pix_width / pixmap.width())
+                )  # calculate the corresponding height
+                scaled_pixmap = pixmap.scaled(
+                    pix_width, height
+                )  # create a new scaled pixmap
 
                 widget.setPixmap(scaled_pixmap)
             except:
