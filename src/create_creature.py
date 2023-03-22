ec8717a464d4c793ad2b6adc99e6058dc44335a4
tdnbo <46909851+tdanbo@users.noreply.github.com>
2023-03-22 22:15:21 +0100

    .

---
+++ src/create_creature.py
@@ -0,0 +1,12 @@
+class Creature:
+    def __init__(self, creature_type, resistance):
+        
+        self.creature_type = creature_type
+        self.resistance = resistance
+
+
+    def get_name():
+
+    def get_xp():
+
+creature1 = Creature("Beast","Strong")
